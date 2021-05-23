c1f0874ed88b82b9b7e93018b9101cad9ed0e3ff
Jeff <rustyangel@gmail.com>
2021-05-22 22:48:23 -0600

    New: Add Tailwind config & initial layout styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,9 @@ export default {
             { hid: 'description', name: 'description', content: '' },
         ],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        bodyAttrs: {
+            class: 'bg-grey-900 font-sans text-white',
+        },
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        'components/**/*.{vue,js}',
+        'layouts/**/*.vue',
+        'pages/**/*.vue',
+        'plugins/**/*.{js,ts}',
+        'nuxt.config.{js,ts}',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontMetrics: {
+            sans: {
+                capHeight: 2048,
+                ascent: 2728,
+                descent: -680,
+                lineGap: 0,
+                unitsPerEm: 2816,
+            },
+        },
+
+        extend: {
+            colors: {
+                grey: {
+                    100: '#f1f2f2',
+                    200: '#e7ebed',
+                    300: '#dee4e8',
+                    400: '#c0c8cd',
+                    500: '#949ca1',
+                    600: '#62696d',
+                    700: '#393e41',
+                    800: '#202326',
+                    900: '#16191b',
+                },
+            },
+            fontFamily: {
+                sans: 'Inter, sans-serif',
+            },
+            boxShadow: {
+                'inset-white': 'inset 0 0 0 2px #fff',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require('tailwindcss-capsize').default],
+}
