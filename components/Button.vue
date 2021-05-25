cecdb93c9f58823093cec62959d7cb43903b09f1
Jeff <rustyangel@gmail.com>
2021-05-24 21:03:05 -0600

    Update: Tweak button bg gradient

--- components/Button.vue
+++ components/Button.vue
@@ -1,16 +1,37 @@
 <template>
     <button
-        class="group relative bg-transparent border-0 rounded-2xl appearance-none outline-none"
+        class="
+            group
+            relative
+            bg-transparent
+            border-0
+            rounded-2xl
+            appearance-none
+            outline-none
+        "
         @click="onclick"
     >
         <div class="relative z-10 py-3 lg:py-4 px-10 lg:px-14">
             <span
-                class="lg:text-lg font-medium text-white tracking-widest uppercase"
+                class="
+                    lg:text-lg
+                    font-medium
+                    text-white
+                    tracking-widest
+                    uppercase
+                "
                 >Drop in</span
             >
         </div>
         <div
-            class="absolute inset-0 rounded-2xl bg-gradient-to-r from-[#45D8FD] to-[#364676] transform translate-y-[6px] translate-x-2"
+            :class="$style.bg"
+            class="
+                absolute
+                inset-0
+                rounded-2xl
+                transform
+                translate-y-[6px] translate-x-2
+            "
         />
         <div class="absolute inset-0 rounded-2xl shadow-inset-white" />
     </button>
@@ -28,3 +49,18 @@ export default Vue.extend({
     },
 })
 </script>
+
+<style module>
+.bg {
+    background-image: linear-gradient(
+        to right,
+        #45d8fd 0%,
+        #38bef3 16%,
+        #2ea4e7 33%,
+        #2589da 50%,
+        #2c72be 66%,
+        #325b9d 83%,
+        #364676 100%
+    );
+}
+</style>
