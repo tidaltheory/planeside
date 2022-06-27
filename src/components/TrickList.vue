5f8d25e7c4ee48e28dc91dbb3d5e88f559ae01a5
Jeff <rustyangel@gmail.com>
2022-06-26 22:24:27 -0600

    Fix: Animate Trick text (#44)

---
+++ src/components/TrickList.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import type { TrickObject } from '~/pages/index.vue'
+import TrickText from './TrickText.vue'
+
+interface Props {
+	tricks: Array<TrickObject>
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+	<transition-group name="list">
+		<trick-text
+			v-for="trick in props.tricks"
+			:key="trick.id"
+			:data-id="trick.id"
+		>
+			{{ trick.text }}
+		</trick-text>
+	</transition-group>
+</template>
+
+<style>
+.list-move,
+.list-enter-active {
+	transition: transform 200ms ease, opacity 200ms ease;
+}
+
+.list-leave-active {
+	width: 100%;
+	position: absolute;
+	transition: all 100ms ease;
+}
+
+.list-enter-from {
+	opacity: 0;
+	transform: translateY(2rem);
+}
+
+.list-leave-from {
+	top: 0;
+}
+
+.list-leave-to {
+	opacity: 0;
+	top: 0;
+}
+</style>
