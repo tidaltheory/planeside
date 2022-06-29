38e9482366c2146dde62350a347d952440ad9844
Jeff <rustyangel@gmail.com>
2022-06-29 08:42:46 -0600

    Add Slugblaster text to layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,7 +5,22 @@
 	>
 		<router-view />
 		<div class="relative row-start-1 md:col-start-2">
-			<div class="sticky top-0 h-full bg-black"></div>
+			<div
+				class="sticky top-0 h-full bg-black bg-cover bg-right"
+				:class="$style.image"
+			></div>
+		</div>
+		<div
+			class="pointer-events-none fixed -top-[13vw] w-full select-none text-center uppercase text-white font-amatic text-[20vw] leading-none"
+			aria-hidden
+		>
+			Slugblaster
+		</div>
+		<div
+			class="pointer-events-none fixed -bottom-[13vw] w-full select-none text-center uppercase text-white font-amatic text-[20vw] leading-none"
+			aria-hidden
+		>
+			Slugblaster
 		</div>
 	</div>
 </template>
@@ -14,5 +29,9 @@
 .container {
 	grid-template-rows: 25vh auto;
 	@apply md:grid-rows-1;
+}
+
+.image {
+	background-image: url('/cosmos.jpg');
 }
 </style>
