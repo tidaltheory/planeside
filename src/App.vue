2c588df8786e4ed2872f85891d701aa8ab11a468
Jeff <rustyangel@gmail.com>
2022-07-15 05:42:37 -0600

    Add social meta tags

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head'
 
-// https://github.com/vueuse/head
-// you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
+const DESCRIPTION =
+	'A hoverboard trick generator for the Slugblaster RPG — do more than just kickflip over that quantum centipede!'
+
+/**
+ * https://github.com/vueuse/head
+ * You can use this to manipulate the document head in any components, they will
+ * be rendered correctly in the html results with vite-ssg.
+ */
 useHead({
 	title: 'Planeside',
-	meta: [{ name: 'description', content: '' }],
+	meta: [
+		{ name: 'description', content: DESCRIPTION },
+		{ property: 'og:title', content: 'Planeside' },
+		{ property: 'og:site_name', content: 'Planeside' },
+		{ property: 'og:description', content: DESCRIPTION },
+	],
 })
 </script>
 
